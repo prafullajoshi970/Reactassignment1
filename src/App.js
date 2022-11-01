59cb403204e6d50e713ec329c94fb0befa69e9c5
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2022-11-01 19:57:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione cum exercitationem modi neque minus tempore animi porro natus maiores alias, blanditiis sint inventore beatae unde quam saepe, reiciendis ipsam corrupti?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos velit voluptate sequi itaque alias, dignissimos sapiente corporis enim id quo laudantium natus vero error omnis doloremque saepe, modi non vitae vel beatae ut. Quaerat eos voluptatum minima, molestiae quibusdam consectetur recusandae ea excepturi atque ducimus numquam fuga delectus amet sequi? Mollitia ratione reprehenderit nam labore alias quaerat esse laboriosam sapiente?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, commodi. Et repudiandae iusto unde nam sapiente explicabo quisquam non tenetur dicta, quam, sequi laborum, ab minima sint! Amet, corrupti modi?</p>
+  </div>
   );
 }
 
